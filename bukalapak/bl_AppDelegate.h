c211f25e4e82293bf39107f713b70a4b749a67c1
Arie <arie@wego.com>
2013-10-01 15:50:40 +0700

    Initial Commit

---
+++ bukalapak/bl_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  bl_AppDelegate.h
+//  bukalapak
+//
+//  Created by Arie on 10/1/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bl_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bukalapak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bukalapak
+//
+//  Created by Arie on 10/1/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "bl_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([bl_AppDelegate class]));
+	}
+}
